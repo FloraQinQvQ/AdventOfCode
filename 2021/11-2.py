d470e4da53ca6aba4b58bda3396a2d2c6a5e1d35
FloraQinQvQ <weimeng.qin18@gmail.com>
2021-12-11 11:33:06 +0000

    add day 11

--- 2021/11-2.py
+++ 2021/11-2.py
@@ -1,3 +1,61 @@
-f = open('./2021/inputs/11-sample.txt')
+from pprint import pprint
+import itertools
+
+# f = open('./2021/inputs/11-sample.txt')
 f = open('./2021/inputs/11.txt')
 lines = f.read().splitlines()
+
+row = len(lines)
+col = len(lines[0].strip())
+
+matrix = [[0 for _ in range(col)] for _ in range(row)]
+
+for i, line in enumerate(lines):
+    for j, ele in enumerate(line.strip()):
+        matrix[i][j] = int(ele)
+
+directions = [[-1, 0], [1, 0], [0, -1],
+              [0, 1],
+              [1, 1], [-1, -1], [1, -1], [-1, 1]]
+
+flashes = 0
+
+
+def count_flashes(matrix, i, j):
+    global flashes
+    flashes += 1
+    matrix[i][j] = -1
+
+    for direction in directions:
+        ii = i + direction[0]
+        jj = j + direction[1]
+        if ii < 0 or jj < 0 or ii > len(matrix) - 1 or jj > len(matrix[0]) - 1 or matrix[ii][jj] == -1:
+            continue
+        else:
+            matrix[ii][jj] += 1
+            if matrix[ii][jj] > 9:
+                matrix[ii][jj] = -1
+                count_flashes(matrix, ii, jj)
+    return flashes
+
+
+for step in range(10000):
+    for i in range(row):
+        for j in range(col):
+            matrix[i][j] += 1
+
+    for i in range(row):
+        for j in range(col):
+            if matrix[i][j] > 9 and matrix[i][j] != -1:
+                count_flashes(matrix, i, j)
+
+    for i in range(row):
+        for j in range(col):
+            if matrix[i][j] == -1:
+                matrix[i][j] = 0
+
+    if all(x == 0 for x in itertools.chain(*matrix)):
+        break
+
+pprint(matrix)
+pprint(step + 1)
